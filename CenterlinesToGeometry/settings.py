0ee925a9a72b37f98223ca843d5ef24679756e16
Bruno Gouvêa <bruno.gv.br@gmail.com>
2017-06-23 12:53:09 -0300

    Commit

--- CenterlinesToGeometry/settings.py
+++ CenterlinesToGeometry/settings.py
@@ -1,7 +1,7 @@
 import cubit
 
-#scale = 100000
-scale = 1
-#folder = "/home/bruno/UFJF-PROJETO/ArvoreN4/"
+scale = 10000
+#scale = 1
+folder = "/home/bruno/UFJF-PROJETO/ArvoreN4/"
 #folder = "/home/bruno/UFJF-PROJETO/ArvoreN25/"
-folder = "/home/bruno/artigo/"
+#folder = "/home/bruno/artigo/"
